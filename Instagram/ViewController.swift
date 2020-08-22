41a6f3e983e30587628088a5f64ea36e6a3d6a39
potatocat123 <akshara.mantha07@rafflesgirlssch.edu.sg>
2020-08-22 09:48:32 +0800

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Akshara Mantha on 22/8/20.
+//  Copyright © 2020 Swift Accelerator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
